9dff3014c8b4885a3774ee481ccff94cbd8bcdb3
lidav <lidav6@cs.washington.edu>
2016-10-26 16:36:50 -0700

    Created all modules and some documentation for Alert

--- app/src/main/java/controllers/OBAController.java
+++ app/src/main/java/controllers/OBAController.java
@@ -1,8 +1,23 @@
 package controllers;
 
+import java.util.Set;
+import modules.*;
+
 /**
  * Created by lidav on 10/23/2016.
+ *
+ * Controller that receives information from OneBusAway
  */
 
 public class OBAController {
+    /**
+     * Contructs an OBAController and connects to OBA API
+     */
+    public OBAController() {
+        throw new UnsupportedOperationException("Not Yet Implemented");
+    }
+
+    public Set<Route> getRoutes() {
+        throw new UnsupportedOperationException("Not Yet Implemented");
+    }
 }
