0be8d8ddaefeb739428ddd34c9aed4d8438163d1
Bryce Martz <bmartz@cs.washington.edu>
2016-10-26 20:43:24 -0700

    Added method stubs for alert display screen

--- app/src/main/java/com/wheresmybus/AlertDisplay.java
+++ app/src/main/java/com/wheresmybus/AlertDisplay.java
@@ -1,8 +1,38 @@
 package com.wheresmybus;
 
+import java.util.*;
+import modules.Alert;
+import modules.Neighborhood;
+
 /**
  * Created by lidav on 10/23/2016.
  */
 
 public class AlertDisplay {
+
+    /**
+     * Gets the alerts for a particular route
+     * @param routeNumber the number of the route (as an int) to get alerts for
+     * @return the list of alerts associated with the given route
+     */
+    public List<Alert> getAlertsForRoute(int routeNumber) {
+        throw new UnsupportedOperationException("Not Yet Implemented");
+    }
+
+    /**
+     * Gets the alerts for a particular neighborhood
+     * @param neighborhood the neighborhood to get alerts for
+     * @return the list of alerts associated with the given neighborhood
+     */
+    public List<Alert> getAlertsForNeighborhood(Neighborhood neighborhood) {
+        throw new UnsupportedOperationException("Not Yet Implemented");
+    }
+
+    /**
+     * Display the details for an alert on selection
+     * @return true if the selection succeeds, false otherwise
+     */
+    public boolean onAlertSelect() {
+        throw new UnsupportedOperationException("Not Yet Implemented");
+    }
 }
