feca61370f65629332beb7a602dcb24c2834887f
Becky Leslie <beleslie@cs.uw.edu>
2016-11-03 10:14:20 -0700

    Merge branch 'master' of https://github.com/WheresMyBus/android
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/wheresmybus/Splash.java
+++ app/src/main/java/com/wheresmybus/Splash.java
@@ -4,16 +4,36 @@ package com.wheresmybus;
  * Created by lesli_000 on 11/1/2016.
  */
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.content.Intent;
+import com.wheresmybus.R;
 
-public class Splash extends Activity {
+public class Splash extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.AppTheme);
+        //setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+
+        /*
+        Thread thread = new Thread(){
+            @Override
+            public void run(){
+                try {
+                    sleep(3000);
+                    Intent startMainScreen = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(startMainScreen);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+        */
+
     }
 }
