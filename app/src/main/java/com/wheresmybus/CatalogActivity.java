02fd64f14dd86cd5166ef2eec44eb8cd01abd315
Bryce Martz <bmartz@cs.washington.edu>
2016-11-03 20:39:00 -0700

    Added default elements to routes list for testing

--- app/src/main/java/com/wheresmybus/CatalogActivity.java
+++ app/src/main/java/com/wheresmybus/CatalogActivity.java
@@ -2,12 +2,21 @@ package com.wheresmybus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class CatalogActivity extends AppCompatActivity {
+
+    private String[] routeNames = {"44 - Wallingford", "70 - Downtown"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_catalog, routeNames);
+
+        ListView listView = (ListView) findViewById(R.id.routeNames);
+        listView.setAdapter(adapter);
     }
 }
