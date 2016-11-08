fa515cb9fb655bde6a00a81b591435dc096ca364
Becky Leslie <beleslie@cs.uw.edu>
2016-11-08 14:11:49 -0800

    added text and spinner for bus routes to submit alert layout

--- app/src/main/java/com/wheresmybus/SubmitAlertActivity.java
+++ app/src/main/java/com/wheresmybus/SubmitAlertActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.RadioButton;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 /**
  * Created by lesli_000 on 11/8/2016.
@@ -13,11 +15,29 @@ import android.widget.Button;
 
 public class SubmitAlertActivity extends AppCompatActivity {
     private Button submitButton;
+    private Spinner busRouteSpinner;
+    private Spinner neighborhoodSpinner;
+    private TextView busRoute;
+    private TextView neighborhood;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit_alert);
+
+        busRoute = (TextView) findViewById(R.id.choose_bus_route);
+        busRoute.setVisibility(View.INVISIBLE);
+
+        busRouteSpinner = (Spinner) findViewById(R.id.bus_route_spinner);
+        busRouteSpinner.setVisibility(View.INVISIBLE);
+
+        // neighborhood = (TextView) findViewById(R.id.choose_neighborhood);
+        //neighborhood.setVisibility(View.INVISIBLE);
+
+        // neighborhoodSpinner = (Spinner) findViewById(R.id.neighborhood_spinner);
+        //neighborhoodSpinner.setVisibility(View.INVISIBLE);
+
+        // add these things for selecting tag for alert and text box for alert description
 
         submitButton = (Button) findViewById(R.id.button_submit);
         submitButton.setVisibility(View.INVISIBLE);
@@ -33,12 +53,15 @@ public class SubmitAlertActivity extends AppCompatActivity {
                 if (checked)
                     // Pirates are the best
 
-                    submitButton.setVisibility(View.VISIBLE);
+                    busRoute.setVisibility(View.VISIBLE);
+                    busRouteSpinner.setVisibility(View.VISIBLE);
                     break;
             case R.id.radio_neighborhood:
                 if (checked)
                     // Ninjas rule
 
+                    busRoute.setVisibility(View.INVISIBLE);
+                    busRouteSpinner.setVisibility(View.INVISIBLE);
                     submitButton.setVisibility(View.VISIBLE);
                     break;
         }
