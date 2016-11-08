bd65daf1f8e9e5c3eac2e2cb71e97757c8ac8602
Becky Leslie <beleslie@cs.uw.edu>
2016-11-08 11:45:49 -0800

    testing how to set visibility of layout components in submit alert screen

--- app/src/main/java/com/wheresmybus/SubmitAlertActivity.java
+++ app/src/main/java/com/wheresmybus/SubmitAlertActivity.java
@@ -5,17 +5,22 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.RadioButton;
 import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by lesli_000 on 11/8/2016.
  */
 
 public class SubmitAlertActivity extends AppCompatActivity {
+    private Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit_alert);
+
+        submitButton = (Button) findViewById(R.id.button_submit);
+        submitButton.setVisibility(View.INVISIBLE);
     }
 
     public void onRadioButtonClicked(View view) {
@@ -27,10 +32,14 @@ public class SubmitAlertActivity extends AppCompatActivity {
             case R.id.radio_bus_route:
                 if (checked)
                     // Pirates are the best
+
+                    submitButton.setVisibility(View.VISIBLE);
                     break;
             case R.id.radio_neighborhood:
                 if (checked)
                     // Ninjas rule
+
+                    submitButton.setVisibility(View.VISIBLE);
                     break;
         }
     }
