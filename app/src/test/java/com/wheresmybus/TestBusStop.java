3b32bbb507500d43b90c458bcd954544293b4942
lidav <lidav6@cs.washington.edu>
2016-11-11 12:55:37 -0800

    tests for BusStop and Neighborhood

---
+++ app/src/test/java/com/wheresmybus/TestBusStop.java
@@ -0,0 +1,79 @@
+package com.wheresmybus;
+
+import android.util.Pair;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import modules.Bus;
+import modules.BusStop;
+import modules.Route;
+import static junit.framework.Assert.*;
+
+/**
+ * Unit tests for BusStop.java
+ * Created by lidav on 11/11/2016.
+ */
+
+public class TestBusStop {
+    /**
+     * Tests that getCoordinates() stores and retrieves data correctly
+     */
+    @Test
+    public void testBusStopGetCoords() {
+        BusStop stop = new BusStop(new Pair<>(0.,1.), new HashSet<Route>());
+        assertTrue(stop.getCoordinates() != null);
+        assertTrue(stop.getCoordinates().first == 0.0);
+        assertTrue(stop.getCoordinates().second == 1.0);
+    }
+
+    /**
+     * Tests the getRoutes() stores and retrieves data correctly
+     * Ensures RoutesSet cannot be changed by adding to the outside Set
+     */
+    @Test
+    public void testGetRoutes() {
+        Set<Route> routes = new HashSet<>();
+        routes.add(new Route("1", "a", "b"));
+        BusStop stop = new BusStop(new Pair<>(0.,1.), routes);
+        assertTrue(stop.getRoutes() != null);
+        assertTrue(stop.getRoutes().size() == 1);
+
+        routes.add(new Route("2", "c", "d"));
+        assertTrue(stop.getRoutes().size() == 1);
+    }
+
+    @Test
+    public void testSetId(){
+        BusStop sampleBus = new BusStop(new Pair<>(0.,1.), new HashSet<Route>());
+        assertEquals(-1, sampleBus.getId());
+        assertTrue(sampleBus.setId(2));
+        assertEquals(sampleBus.getId(), 2);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullCoords() {
+        BusStop stop = new BusStop(null, new HashSet<Route>());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullRoutes() {
+        BusStop stop = new BusStop(new Pair<>(0.,1.), null);
+    }
+
+    @Test
+    public void testGetId() {
+        BusStop sampleBus = new BusStop(new Pair<>(0.,1.), new HashSet<Route>());
+        boolean b = sampleBus.setId(2);
+        assertTrue(b);
+        assertTrue(sampleBus.getId() == 2);
+
+        b = sampleBus.setId(3);
+        assertFalse(b);
+        assertTrue(sampleBus.getId() == 2);
+    }
+}
