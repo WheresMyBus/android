918041ac749af388a94d6ec232bbc9047416125d
nbissiri <nbissiri@localhost.localdomain>
2016-11-11 19:35:57 -0800

    build.sh finalized

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 echo "Building debug APK using Adroid SDK located at: $1"
-sudo sed -i -e 's//http\:\/\/localhost\:5000\//http\:\/\/wheresmybus-api\.herokuapp\.com\/g' app/src/main/java/controllers/WMBController.java
+sudo sed -i -e 's/http\:\/\/localhost\:5000\//http\:\/\/wheresmybus-api\.herokuapp\.com\//g' app/src/main/java/controllers/WMBController.java
 export ANDROID_HOME=$1
 chmod u+x gradlew
 gradlew assembleDebug
