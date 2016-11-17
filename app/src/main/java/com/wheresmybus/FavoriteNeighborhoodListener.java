17174d59132d1d3dd45c569213949fb383a95ca6
Nicolas Bissiri <nbissiri@uw.edu>
2016-11-16 22:09:59 -0800

    Users can now toggle favorites on and off

--- app/src/main/java/com/wheresmybus/FavoriteNeighborhoodListener.java
+++ app/src/main/java/com/wheresmybus/FavoriteNeighborhoodListener.java
@@ -11,10 +11,12 @@ import android.widget.ImageButton;
 public class FavoriteNeighborhoodListener implements View.OnClickListener {
     private int neighborhoodID;
     private CatalogActivity catalogActivity;
+    private boolean toggledOn;
 
     public FavoriteNeighborhoodListener(int neighborhoodID, CatalogActivity catalogActivity) {
         this.neighborhoodID = neighborhoodID;
         this.catalogActivity = catalogActivity;
+        this.toggledOn = false;
     }
 
     @Override
@@ -22,10 +24,16 @@ public class FavoriteNeighborhoodListener implements View.OnClickListener {
         // add to or remove from favorites
         // change color of star button
         ImageButton button = (ImageButton) v.findViewById(R.id.star);
-        button.setColorFilter(ContextCompat.getColor(v.getContext(), R.color.yellow));
-        // when go back to gray, use button.clearColorFilter();
-
-        // to get the user ID: Secure.getString(getApplicationContext().getContentResolver(), Secure.ANDROID_ID);
-        catalogActivity.favoriteNeighborhoodsByID.add(neighborhoodID);
+        if (toggledOn) {
+            // go back to grey
+            button.clearColorFilter();
+            catalogActivity.favoriteNeighborhoodsByID.remove(neighborhoodID);
+        } else {
+            button.setColorFilter(ContextCompat.getColor(v.getContext(), R.color.yellow));
+            // when go back to gray, use button.clearColorFilter();
+            // to get the user ID: Secure.getString(getApplicationContext().getContentResolver(), Secure.ANDROID_ID);
+            catalogActivity.favoriteNeighborhoodsByID.add(neighborhoodID);
+        }
+        toggledOn = !toggledOn;
     }
 }
