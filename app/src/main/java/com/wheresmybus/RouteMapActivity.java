0889d9c6c6043b2f2ab22e1bcdad895bcf56b03a
Becky Leslie <beleslie@cs.uw.edu>
2016-11-21 15:43:08 -0800

    removed unused imports from RouteMapActivity

--- app/src/main/java/com/wheresmybus/RouteMapActivity.java
+++ app/src/main/java/com/wheresmybus/RouteMapActivity.java
@@ -1,7 +1,6 @@
 package com.wheresmybus;
 
 import android.Manifest;
-import android.app.Fragment;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -9,7 +8,6 @@ import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -62,7 +60,7 @@ public class RouteMapActivity extends FragmentActivity
     private List<Marker> currentBusMarkers;
 
     private final LatLng SEATTLE = new LatLng(47.608013, -122.335167);
-    private final float MARKER_HUE = 288;           // makes the bus markers purple
+    private final float MARKER_HUE = 288;               // makes the bus markers purple
     private final float ZOOM_LEVEL = 18;                // goes up to 21
     private final int REQUEST_LOCATION = 0;
 
