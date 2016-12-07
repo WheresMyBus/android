001c45661ec15c171e65ce5ddd048dc4ca35480a
Bryce Martz <bmartz@cs.washington.edu>
2016-12-06 17:54:54 -0800

    Finished adding menu to submit comment page

--- app/src/main/java/com/wheresmybus/SubmitCommentActivity.java
+++ app/src/main/java/com/wheresmybus/SubmitCommentActivity.java
@@ -1,11 +1,14 @@
 package com.wheresmybus;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -33,6 +36,7 @@ public class SubmitCommentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit_comment);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -45,6 +49,44 @@ public class SubmitCommentActivity extends AppCompatActivity {
             isRouteAlert = false;
             neighborhoodAlert = (NeighborhoodAlert) alert;
         }
+    }
+
+    /**
+     * Creates an options menu.
+     *
+     * @param menu the menu to be inflated
+     * @return true
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    /**
+     * Handles the event when an option is selected from the options menu.
+     *
+     * @param item The selected item
+     * @return false to allow normal menu processing to proceed, true to consume it here
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_help) {
+            Uri uri = Uri.parse("https://github.com/WheresMyBus/android/wiki/User-Documentation");
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     /**
